24bcc631779751c9008a70f52b9e25dd6cfa47f8
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-13 22:48:47 -0600

    variables - 4

--- 01.introducción-a-javascript/js/02.variables.js
+++ 01.introducción-a-javascript/js/02.variables.js
@@ -7,3 +7,7 @@ console.log("numero", numero);
 //Variables de Texto
 var palabra = 'Joan';
 console.log("palabra", palabra);
+
+//Variables Boleanas
+var boleana = true;
+console.log("boleana", boleana);
