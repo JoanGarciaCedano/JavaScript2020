b016b1ef47efac33ee1a29c926180110452afff7
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-14 23:02:07 -0600

    BASICS - Strings 3

--- 01.introducción-a-javascript/BASICS/strings.js
+++ 01.introducción-a-javascript/BASICS/strings.js
@@ -7,3 +7,11 @@ let lastName = 'García';
 let fullName = firstName + ' ' + lastName;
 
 console.log(fullName);
+
+//challenge
+
+let city = 'Tepic';
+let country = 'Mexico';
+let location = city + ' ' + country;
+
+console.log(location);
