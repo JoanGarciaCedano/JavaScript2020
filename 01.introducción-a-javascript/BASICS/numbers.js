b16dd5c93ad9b8fb33db34767bb079f96a008214
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-14 23:15:20 -0600

    BASICS - Numbers Challenge

--- 01.introducción-a-javascript/BASICS/numbers.js
+++ 01.introducción-a-javascript/BASICS/numbers.js
@@ -1,5 +1,21 @@
 // let num = 5.5 - -1;
-let x = 11;
-let num = x + 1 * 2;
+// let x = 11;
+// let num = x + 1 * 2;
 
-console.log(num);
+// console.log(num);
+
+let age = 26
+let dogYears = (age + 1) / 7;
+
+console.log(dogYears);
+
+//challenge
+
+//studentScore 18
+studentScore = 18
+//maxScore 20
+maxScore = 20
+//percent 90
+percent = (studentScore / maxScore) * 100;
+//print the percent
+console.log('My percent is: ' + percent + "%");
