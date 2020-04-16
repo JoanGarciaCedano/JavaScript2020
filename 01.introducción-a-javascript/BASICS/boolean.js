8015a800282cdf9ce20a86ccf579efec1c8c98fe
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-15 21:54:40 -0600

    BASICS - boolean challenge

--- 01.introducción-a-javascript/BASICS/boolean.js
+++ 01.introducción-a-javascript/BASICS/boolean.js
@@ -1,6 +1,24 @@
-// === equality operator
+// === - equality operator
+// !== - not equal operator
+// < - less than operator
+// > - greater than operator
+// <= - less than or equal to operator
+// >= - greater than or equal to operator
 
 let temp = 31;
-let isFreezing = temp !== 32;
+let isFreezing = temp <= 32;
 
 console.log(isFreezing);
+
+//Challenge area
+
+//Create age set your age
+let age = 26
+//Calculate is child - if they are 7 or under
+let isChild = age <= 7;
+//Calculate is senior - if they are 65 or older
+let isSenior = age >= 65;
+//Print is child value
+console.log("is he child? " + isChild);
+//Print is senior value
+console.log("is he Senior? " + isSenior);
