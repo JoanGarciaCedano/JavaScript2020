d3a383e963971b1d55360a80662ca3caa907374b
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-15 21:20:29 -0600

    BASICS - variables 4

--- 01.introducción-a-javascript/BASICS/variables.js
+++ 01.introducción-a-javascript/BASICS/variables.js
@@ -7,3 +7,7 @@ console.log(petName);
 //2. There are rules related to variable names
 let test_ = 2
 let result = 3 + 4;
+
+//3. Variable names cannot be reserved keywords
+let let = 12;
+console.log(let);
