e5e836eae6fb0de93bd558fad95eb04d1d263d01
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-22 22:10:37 -0600

    BASICS - logical-and-or 2

--- 01.introducción-a-javascript/BASICS/logical-and-or.js
+++ 01.introducción-a-javascript/BASICS/logical-and-or.js
@@ -1,7 +1,18 @@
 let temp = 65
 
-if (temp >= 60) {
-    if(tmep <= 90) {
+// if (temp >= 60) {
+//     if(tmep <= 90) {
         
-    }
+//     }
+// }
+
+// Logical And Operator - Ture if both sides are true. False otherwise
+// Logical Or Operator - True if  at least one side is true. False otherwise
+
+if (temp >= 60 && temp <= 90) {
+    console.log('It is pretty nice out')
+} else if (temp <= 0 || temp >= 120) {
+    console.log('Do not go outside')
+} else {
+    console.log('Eh. Do what you want')
 }
