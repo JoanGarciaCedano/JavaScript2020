ebd2863b4268115f1e242756ed7d3d3a0afa41c1
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-22 21:51:44 -0600

    BASICS - boolean-advanced challenge 2

--- 01.introducción-a-javascript/BASICS/boolean-advanced.js
+++ 01.introducción-a-javascript/BASICS/boolean-advanced.js
@@ -12,14 +12,14 @@ if (isAccountLocked) {
 
 // Challenge aea
 
-let temp = 45
+let temp = 45;
 
 // It is freezing outside!
 if (temp <= 32){
-    console.log('It is freezing outside!')
+    console.log('It is freezing outside!');
 // It is hot outside!
-}else if (temp => 110){
-    console.log('It is hot outside!')
+}else if (temp >= 110){
+    console.log('It is hot outside!');
 // Go for it. It is pretty nice.
 }else{
     console.log("Go for it. It is pretty nice.")
