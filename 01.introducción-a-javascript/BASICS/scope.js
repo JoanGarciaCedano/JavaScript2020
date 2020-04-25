9d9a9916263748c00978294b631e86b2208e858f
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-24 21:58:10 -0600

    BASICS - scope.js 2

--- 01.introducción-a-javascript/BASICS/scope.js
+++ 01.introducción-a-javascript/BASICS/scope.js
@@ -1,8 +1,13 @@
+// Lexical Scope (Static Scope)
+// Global Scope - Defined outisde of all code blocks
+// Local Scope - Defined inside a code block
+
 let varOne ='varOne'
 
 if (true) {
     console.log(varOne)
     let varTwo = 'varTwo'
+    console.log(varTwo)
 }
 
 console.log(varTwo)
