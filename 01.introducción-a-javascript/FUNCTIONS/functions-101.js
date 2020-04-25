71bffee683408b5717109fea958cb148952fa625
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-24 22:34:06 -0600

    FUNCTIONS - functions-101.js challenge

--- 01.introducción-a-javascript/FUNCTIONS/functions-101.js
+++ 01.introducción-a-javascript/FUNCTIONS/functions-101.js
@@ -18,3 +18,23 @@ let otherValue = square(10)
 
 console.log(value)
 console.log(otherValue)
+
+// Challenge Area
+
+//convertFahrenheitToCelsius
+
+let fahrenheit = 32; // 0c - 273.15k
+let celsius = function (fahrenheit) {
+    let result = (fahrenheit - 32) * 5 / 9
+    return result
+}
+
+let kelvin = function (fahrenheit) {
+    let result = (fahrenheit + 459.670) * 5 / 9
+    return result
+}
+
+let valueCelcius = celsius(fahrenheit)
+console.log(valueCelcius + "C")
+let valueKelvin = kelvin(fahrenheit)
+console.log(valueKelvin + "k");
