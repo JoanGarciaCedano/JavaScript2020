bf9dd12da6c349c31251fbe53b9228f4c5d389e6
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-26 14:13:21 -0600

    FUNCTIONS - undefined-null.js 2

---
+++ 01.introducción-a-javascript/FUNCTIONS/undefined-null.js
@@ -0,0 +1,10 @@
+let name
+
+if (name === undefined) {
+    console.log("La variable name está sin valor")
+} else {
+    console.log("La variable name tiene el valor: " + name)
+}
+
+
+console.log(name);
