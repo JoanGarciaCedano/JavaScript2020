ce99c97d06daf0eaf04e5236cea12ebfa9681885
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-04-26 14:39:44 -0600

    FUNCTIONS - arguments.js 4

--- 01.introducción-a-javascript/FUNCTIONS/arguments.js
+++ 01.introducción-a-javascript/FUNCTIONS/arguments.js
@@ -1,3 +1,4 @@
+// Multiple arguments
 let add = function (a, b, c) {
     return a + b + c
 }
@@ -5,3 +6,11 @@ let add = function (a, b, c) {
 let result = add(10 , 1 , 5)
 
 console.log(result)
+
+// Default arguments
+let getScoreText = function (name = 'Anonymous', score = 0) {
+    console.log(name)
+    console.log(score)
+}
+
+getScoreText()
