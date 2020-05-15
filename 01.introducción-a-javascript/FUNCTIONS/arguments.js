9f608bc101a457f41d9483270f9b16936bb7bc45
JoanGarciaCedano <joan.gc.mail@gmail.com>
2020-05-15 12:08:36 -0600

    arguments

--- 01.introducción-a-javascript/FUNCTIONS/arguments.js
+++ 01.introducción-a-javascript/FUNCTIONS/arguments.js
@@ -25,4 +25,45 @@ let resultTip = tip(100, 0.5)
 
 console.log("The tip was: $" + result)
 
-//mir-suic-slwfst- dan-rid-io- zul-lif-slw- jos-do-fst guz-dee-hrd
+//mir-suic-slwfst-mal dan-rid-io-gim zul-lif-slw-sil jos-do-fst-ench guz-dee-hrd-gri    DONE
+
+//lizsa-dee-slw-en jhon-do-slwfst-mal mrivl-lif-hrd-gri gabt-suic-fst-sil eun-rid-io-gi DONE
+
+//15yo
+//fat-sui-slw-sil mich-lif-io-en goy-dee-hrd-mal chin-ri-fst-gi col-do-slwfst-gri DONE
+
+//end-to-first
+//ma-ri-slwfs-en pac-do-fst-sil kar-dee-slw-gi  kaz-sui-hrd-mal ja-lif-io-gri DONE
+
+//cry
+//chu-sui-fst-mal pow-lif-io-sil joa-ri-hrd-gi dar-dee-slwfst-gri chori-do-slw-en
+
+//bed
+//tal-sui-io-en alherr-dee-fas-mal bethor-lif-slwfst-gim ros-do-slw-sil vik-ri-hrd-gri
+
+//fks
+//mar
+
+
+
+
+
+//fart
+//deli-lif-fs-sil fan-do-io-en jaq-sui-slw-gri ber-dee-slwfst-mal tze-ri-hrd-gim
+
+
+
+
+
+//skr
+//xo-lif-fst-enc ab-dee-slwfst-gri absi-do-hrd-sil est-sui-io-gim buch-ri-slw-mal
+
+//bigpu
+//percol-sui andr-lif sahe-dee bren-do Wmor-ri
+
+//Leggi
+//Kren sul bub pat perlh
+
+//allforone
+//jose jhon chin 
+
